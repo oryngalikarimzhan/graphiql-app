9f92e64fd678c78ec8ddf050e660bed64621d548
Stsiapan Zubik <stteppa9092@gmail.com>
2023-05-04 19:34:00 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NotFound from './pages/notFound/NotFound';
 import 'normalize.css';
+
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </>
   );
 };
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,27 @@
+import styles from './Footer.module.scss';
+import logoRss from '../../assets/img/logo-rs.svg';
+import iconGithub from '../../assets/img/github-icon.png';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <a href="https://rs.school" target="blank">
+          <img className={styles.logoRss} src={logoRss} alt="rss logo" />
+        </a>
+        <p className={styles.text}>2023</p>
+        <div className={styles.ghContainer}>
+          <a href="https://github.com/IharAnt">
+            <img className={styles.github} src={iconGithub} alt="github icon" />
+          </a>
+          <a href="https://github.com/oryngalikarimzhan">
+            <img className={styles.github} src={iconGithub} alt="github icon" />
+          </a>
+          <a href="https://github.com/Stepan9092">
+            <img className={styles.github} src={iconGithub} alt="github icon" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
