882da7c9d451c34b8e27e3e91ee17c3eaf399aff
IharAnt <the-best@tut.by>
2023-05-04 23:23:04 +0300

    feat: add firebase config

---
+++ src/config/FirebaseConfig.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCNKyUV4m79pbKMW_FJrx3C8RLkvMBzUso',
+  authDomain: 'graphiql-app-5ebc8.firebaseapp.com',
+  projectId: 'graphiql-app-5ebc8',
+  storageBucket: 'graphiql-app-5ebc8.appspot.com',
+  messagingSenderId: '681893458513',
+  appId: '1:681893458513:web:0bc0ad92930c16a4ca3e70',
+  measurementId: 'G-GNQ11KK33H',
+};
+export const app = initializeApp(firebaseConfig);
