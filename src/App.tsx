0ec2cebe5ea4b07cc8e77f77ea9261eb6d57895a
Stsiapan Zubik <stteppa9092@gmail.com>
2023-05-05 18:07:57 +0300

    feat: add header btns, fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,11 @@ import 'normalize.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import NotFound from './pages/notFound/NotFound';
-import { LanguageSelector } from './components/language-selector/LanguageSelector';
 
 const App = () => {
   return (
     <>
       <Header />
-      <LanguageSelector />
       <Routes>
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState, FC } from 'react';
 import classNames from 'classnames';
+import { useTranslation } from 'react-i18next';
 
 import styles from './Header.module.scss';
-import LogoutIcon from '../logoutIcon/LogoutIcon';
+import { LanguageSelector } from '../language-selector/LanguageSelector';
+import { NavLink } from 'react-router-dom';
 
 const Header: FC = () => {
   const [sticky, setSticky] = useState(false);
+  const { t } = useTranslation();
 
   const handleScroll = () => {
     window.pageYOffset > 0 ? setSticky(true) : setSticky(false);
@@ -18,9 +21,19 @@ const Header: FC = () => {
   return (
     <header className={classNames(styles.header, { [styles.sticky]: sticky })}>
       <div className={styles.container}>
-        <nav></nav>
-        <div className={styles.logout}>
-          <LogoutIcon sticky={sticky} />
+        <nav className={styles.nav}>
+          <NavLink className={classNames(styles.navLink, { [styles.stickyText]: sticky })} to={''}>
+            Main Page
+          </NavLink>
+          <NavLink className={classNames(styles.navLink, { [styles.stickyText]: sticky })} to={''}>
+            Welcome Page
+          </NavLink>
+        </nav>
+        <div className={styles.btns}>
+          <LanguageSelector />
+          <div className={styles.logout}>
+            <button className={styles.auth}>{`${t('sign-in')}`}</button>
+          </div>
         </div>
       </div>
     </header>
