c86a23e9889f37ee513b444562ac28bba8ba393a
IharAnt <the-best@tut.by>
2023-05-15 00:44:53 +0300

    feat: add avatar component

--- src/pages/welcomePage/WelcomePage.tsx
+++ src/pages/welcomePage/WelcomePage.tsx
@@ -2,14 +2,19 @@ import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './WelcomePage.module.scss';
 import AuthWelcomeController from '../../components/auth-welcome-controller/AuthWelcomeController';
+import Avatar from '../../components/avatar/Avatar';
+import avatarImage from '../../assets/img/avatar.jpeg';
 
 const WelcomePage: FC = () => {
   const { t } = useTranslation();
 
   return (
     <div className="wrapper">
-      <h1>{t('welcome-msg')}</h1>
+      <h2>Итоговый проект курса RS School React 2022Q3</h2>
       <AuthWelcomeController className={styles.authWelcome} />
+      <div style={{ height: 100, width: 100 }}>
+        <Avatar image={avatarImage}></Avatar>
+      </div>
     </div>
   );
 };
