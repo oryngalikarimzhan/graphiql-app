5ac0b7e7fbb1c4a3c3f6392e5ae54a14b239c865
IharAnt <the-best@tut.by>
2023-05-15 20:21:37 +0300

    feat: add welcome page localization

--- src/components/developer-card/DeveloperCard.tsx
+++ src/components/developer-card/DeveloperCard.tsx
@@ -4,13 +4,14 @@ import styles from './DeveloperCard.module.scss';
 import { IDevCardProps } from './types';
 import Avatar from '../avatar/Avatar';
 
-const DeveloperCard: FC<IDevCardProps> = ({ image, name, avatarLink }) => {
+const DeveloperCard: FC<IDevCardProps> = ({ image, name, avatarLink, description }) => {
   return (
     <div className={styles.cardContainer}>
       <a href={avatarLink} target={'_blank'} rel="noreferrer">
         <Avatar image={image}></Avatar>
       </a>
       <h4>{name}</h4>
+      <div className={styles.developerCardDescription}>{description}</div>
     </div>
   );
 };
--- src/pages/welcomePage/WelcomePage.tsx
+++ src/pages/welcomePage/WelcomePage.tsx
@@ -2,8 +2,7 @@ import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './WelcomePage.module.scss';
 import AuthWelcomeController from '../../components/auth-welcome-controller/AuthWelcomeController';
-import Avatar from '../../components/avatar/Avatar';
-import avatarImage from '../../assets/img/avatar.jpeg';
+import iharImage from '../../assets/img/ihar2.jpg';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../config/FirebaseConfig';
 import DeveloperCard from '../../components/developer-card/DeveloperCard';
@@ -14,32 +13,37 @@ const WelcomePage: FC = () => {
 
   return (
     <div className="wrapper">
-      <h2>Итоговый проект.</h2>
-      <p>{!user && 'Главная страница доступна только зарегистрированным пользователям.'}</p>
+      <h2>{t('final-project.name')}</h2>
+      <p>{!user && t('final-project.description')}</p>
       <AuthWelcomeController className={styles.authWelcome} />
       <div>
-        <h3 className={styles.textUnderline}>Курс: RS School. React.</h3>
-        <p>
-          RS School - это образовательный проект, который проводит курсы по различным направлениям
-          веб-разработки. Один из таких курсов - это курс по React. Курс предназначен для студентов
-          RS School, которые прошли второй этап RS School и новых студентов с практическим опытом и
-          знаниями JavaScript, TypeScript, Git, GitHub, CSS3 / HTML.
-        </p>
+        <h3 className={styles.textUnderline}>{t('course.name')}</h3>
+        <p>{t('course.description')}</p>
       </div>
       <div>
-        <h3 className={styles.textUnderline}>Проект: GraphiQL.</h3>
-        <p>
-          GraphiQL - это интерактивная среда разработки GraphQL в браузере. Она позволяет
-          формировать запросы и исследовать схему GraphQL.
-        </p>
+        <h3 className={styles.textUnderline}>{t('project.name')}</h3>
+        <p>{t('project.description')}</p>
       </div>
       <div>
-        <h3 className={styles.textUnderline}>Разработчики.</h3>
-        <div style={{ height: 200, width: 200 }}>
+        <h3 className={styles.textUnderline}>{t('developers')}</h3>
+        <div className={styles.developersContainer}>
           <DeveloperCard
-            image={avatarImage}
+            image={iharImage}
             name="Ihar Antonenka"
             avatarLink="https://www.linkedin.com/in/ihar-antonenka-ba52371a5/"
+            description={t('developer-card.ihar.description') || ''}
+          />
+          <DeveloperCard
+            image={iharImage}
+            name="Ihar Antonenka"
+            avatarLink="https://www.linkedin.com/in/ihar-antonenka-ba52371a5/"
+            description={t('developer-card.orygali.description') || ''}
+          />
+          <DeveloperCard
+            image={iharImage}
+            name="Ihar Antonenka"
+            avatarLink="https://www.linkedin.com/in/ihar-antonenka-ba52371a5/"
+            description={t('developer-card.stepan.description') || ''}
           />
         </div>
       </div>
