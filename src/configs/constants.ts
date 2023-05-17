ca6f8c1959a0e36aaf225ec0795db8cf98f72af6
Oryngali <oryngali96@gmail.com>
2023-05-17 20:39:41 +0500

    feat: style footer

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,3 +1,5 @@
+import { RegularObject } from './../types/types';
+
 const LOCALIZATION_COOKIE_KEY = 'i18next';
 
 enum Language {
@@ -5,6 +7,12 @@ enum Language {
   EN = 'en',
 }
 
+const githubAuthors: RegularObject = {
+  ihar: 'https://github.com/IharAnt',
+  oryngali: 'https://github.com/oryngalikarimzhan',
+  stepan: 'https://github.com/Stepan9092',
+};
+
 const GRAPHQL_API = 'https://rickandmortyapi.com/graphql';
 
-export { Language, LOCALIZATION_COOKIE_KEY, GRAPHQL_API };
+export { Language, LOCALIZATION_COOKIE_KEY, GRAPHQL_API, githubAuthors };
