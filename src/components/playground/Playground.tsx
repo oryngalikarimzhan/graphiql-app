95b7d6c54f5e7fcd5096d48edda62b4bf50b289c
Stsiapan Zubik <stteppa9092@gmail.com>
2023-05-19 17:56:52 +0300

    feat: add req handler

--- src/components/playground/Playground.tsx
+++ src/components/playground/Playground.tsx
@@ -5,11 +5,14 @@ import { PlaygroundSideBar } from './playground-side-bar/PlaygroundSideBar';
 import { SchemaSection } from './schema-section/SchemaSection';
 import { ResponseSection } from './response-section/ResponseSection';
 import { QuerySection } from './query-section/QuerySection';
+import { useLazyGetDataQuery } from '../../store/api';
 
 export const Playground: FC = () => {
+  const [getData, { isLoading }] = useLazyGetDataQuery({});
+
   return (
     <div className={styles.playground}>
-      <PlaygroundSideBar />
+      <PlaygroundSideBar getData={getData} />
 
       <article className={styles.playgroundContainer}>
         <SchemaSection />
--- src/components/playground/playground-side-bar/PlaygroundSideBar.tsx
+++ src/components/playground/playground-side-bar/PlaygroundSideBar.tsx
@@ -7,9 +7,22 @@ import { ReactComponent as DocsIcon } from '../../../assets/icons/docs-icon.svg'
 import { ReactComponent as ExecutorIcon } from '../../../assets/icons/executor-icon.svg';
 import { ReactComponent as FilledDocsIcon } from '../../../assets/icons/filled-docs-icon.svg';
 
-export const PlaygroundSideBar: FC = () => {
-  const { queryEditorValue, schemaIsOpen } = useAppSelector((state) => state.playground);
+export const PlaygroundSideBar: FC = ({ getData }) => {
+  const { queryEditorValue, schemaIsOpen, variablesEditorValue } = useAppSelector(
+    (state) => state.playground
+  );
   const { setSchemaIsOpen, setResponseEditorValue } = useActions();
+
+  const graphqlApiHandler = async () => {
+    const data = await getData({
+      query: queryEditorValue,
+      variables: variablesEditorValue ? JSON.parse(variablesEditorValue) : '',
+    });
+    if (data.error) {
+      setResponseEditorValue(JSON.stringify(data.error.data, null, '\t'));
+    } else setResponseEditorValue(JSON.stringify(data.data, null, '\t'));
+  };
+
   return (
     <aside className={styles.sideBar}>
       <SquareButton isActive={schemaIsOpen} onClick={() => setSchemaIsOpen(!schemaIsOpen)}>
@@ -19,10 +32,7 @@ export const PlaygroundSideBar: FC = () => {
           <DocsIcon height={22} width={18} />
         )}
       </SquareButton>
-      <SquareButton
-        className={styles.executorButton}
-        onClick={() => setResponseEditorValue(queryEditorValue)}
-      >
+      <SquareButton className={styles.executorButton} onClick={graphqlApiHandler}>
         <ExecutorIcon />
       </SquareButton>
     </aside>
