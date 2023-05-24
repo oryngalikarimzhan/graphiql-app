16b1c6dae4ebfbc871ea0ecb3f3f40d313f41097
Oryngali <oryngali96@gmail.com>
2023-05-25 02:06:55 +0500

    fix: remove unneccassary import in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import 'normalize.css';
 
 import Header from './components/header/Header';
